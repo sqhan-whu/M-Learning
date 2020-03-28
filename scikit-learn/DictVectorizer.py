2c771297075367570757924c47ed3fb266f116c8
Han Shaoqing <502466541@qq.com>
2020-03-28 23:32:49 +0800

    Update DictVectorizer.py

--- scikit-learn/DictVectorizer.py
+++ scikit-learn/DictVectorizer.py
@@ -10,3 +10,22 @@ def dict_demo():
 	print (data_new)
 	print(transfer.get_feature_names())
 dict_demo()
+
+from sklearn.feature_extraction.text import CountVectorizer
+
+def count_demo():
+	"""
+	文本特征提取
+	"""
+	data = ["life is short, i like like python", "life is too long, i dislike python"]
+
+	transfer = CountVectorizer()
+
+	data_new = transfer.fit_transform(data)
+	print("data_new:\n",data_new.toarray())
+	print(transfer.get_feature_names())
+
+	return None
+count_demo()
+
+
