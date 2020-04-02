e4d94a4cc5cd7877c2985c355b9a47a92c1913c9
Han Shaoqing <502466541@qq.com>
2020-04-02 17:02:21 +0800

    Create PCA.py

---
+++ scikit-learn/PCA.py
@@ -0,0 +1,23 @@
+### instacrat Market
+
+import pandas as pd
+order_products = pd.read_csv("order_products__prior.csv")
+products = pd.read_csv("products.csv")
+orders = pd.read_csv("orders.csv")
+aisles = pd.read_csv("aisles.csv")
+
+table1 = pd.merge(orders,order_products,on="order_id")
+table2 = pd.merge(table1,products,on="product_id")
+table3 = pd.merge(table2,aisles,on="aisle_id" )
+
+table = pd.crosstab(table3["user_id"],table3["aisle_id"])
+data = table[:10000]
+#print(table.head())
+#
+from sklearn.decomposition import PCA
+#1. 实例一个转化器类型：
+transfer = PCA(n_components=0.95)
+#2. 调用fit_transform
+data_new = transfer.fit_transform(data)
+
+print(data_new.shape)
