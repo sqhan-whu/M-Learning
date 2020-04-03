a9020680c753f2835d232c19d9319f3b0f3f906c
Han Shaoqing <502466541@qq.com>
2020-04-03 21:07:59 +0800

    Create LogisticRegression.py

---
+++ scikit-learn/LogisticRegression.py
@@ -0,0 +1,38 @@
+import pandas as pd
+import numpy as np
+path = "http://archive.ics.uci.edu/ml/machine-learning-databases/breast-cancer-wisconsin/breast-cancer-wisconsin.data"
+column_name = ['Sample code number','Clump Thickness','Uniformity of Cell Size','Uniformity of Cell Shape','Marginal Adhesion','Single Epithelial Cell Size','Bare Nuclei','Bland Chromatin','Normal Nucleoli','Mitoses','Class']
+data = pd.read_csv(path,names=column_name)
+#替换缺失值
+data = data.replace(to_replace='?',value=np.nan)
+data.dropna(inplace=True)
+data.isnull().any() #所有列不存在缺失值
+
+from sklearn.model_selection import train_test_split
+x = data.iloc[:,1:-1]
+y = data["Class"]
+x_train, x_test, y_train, y_test = train_test_split(x,y, random_state=22)
+
+#标准化：
+from sklearn.preprocessing import StandardScaler
+transfer = StandardScaler()
+x_train = transfer.fit_transform(x_train)
+x_test = transfer.transform(x_test)
+
+#逻辑回归：
+from sklearn.linear_model import LogisticRegression
+estimator = LogisticRegression()
+estimator.fit(x_train,y_train)
+
+#逻辑回归模型参数：
+#得出模型
+print("正规方程-权重系数为：\n", estimator.coef_)
+print("正规方程-偏置为：\n", estimator.intercept_)
+
+#模型评估：
+y_predict = estimator.predict(x_test)
+print("y_predict:\n",y_predict)
+print("直接比对真实值和预测值：\n",y_test==y_predict)
+score= estimator.score(x_test,y_test)
+print("准确率:\n",score)
+
